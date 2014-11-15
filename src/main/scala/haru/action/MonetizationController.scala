fa59ba00fdd7800a67ac62cf2b82389757e57768
parkjungun <pheadra4@gmail.com>
2014-11-16 00:15:18 +0900

    수익 관련 백앤드..

---
+++ src/main/scala/haru/action/MonetizationController.scala
@@ -0,0 +1,93 @@
+package haru.action
+
+import java.net.URLEncoder
+
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
+
+import haru.dao.MonetizationDao
+import spray.json._
+import spray.json.DefaultJsonProtocol
+import xitrum.annotation.GET
+//{"results":[],"count":2}
+
+case class UserCount(results: Option[Any], count: Int)
+object UserCountProtocol extends DefaultJsonProtocol {
+  implicit object ColorJsonFormat extends RootJsonFormat[UserCount] {
+    def write(c: UserCount) = JsObject(
+      "count" -> JsNumber(c.count))
+    def read(value: JsValue) = {
+      value.asJsObject.getFields("results", "count") match {
+        case Seq(_, JsNumber(count)) =>
+          new UserCount(null, count.toInt)
+        case _ => throw new DeserializationException("Color expected")
+      }
+    }
+  }
+}
+
+@GET("/monetization")
+class GetMonetization extends Api2 {
+
+  import UserCountProtocol._
+
+  def execute() {
+    val appid = param[String]("appid")
+    val startday = param[String]("startday")
+    val endday = param[String]("endday")
+    val nation = param[String]("nation")
+
+    // send the post request
+    val url = "http://api.haru.io/1/classes/Users?limit=0&count=1&where={}";
+    val safeUrl = "http://api.haru.io/1/classes/Users?limit=0&count=1&where=" + URLEncoder.encode("{}", "UTF-8");
+    println(safeUrl);
+    val getrequeset = new HttpGet(safeUrl)
+
+    getrequeset.addHeader("Application-Id", appid)
+    getrequeset.addHeader("Content-Type", "application/json")
+
+    val requesetConfig = RequestConfig.custom().build()
+
+    val client = HttpClientBuilder.create().build();
+    val response = client.execute(getrequeset)
+    val entity = response.getEntity()
+    System.out.println(response.getStatusLine());
+
+    var content = ""
+    if (entity != null) {
+      val inputStream = entity.getContent()
+      content = scala.io.Source.fromInputStream(inputStream).getLines.mkString
+      println(content);
+      inputStream.close
+    }
+    client.close();
+
+    import PushtotalProtocol._
+    val jsoncontent = content.parseJson
+    val contentjson = jsoncontent.convertTo[UserCount]
+    val monetization = MonetizationDao.GetMonetization(appid, startday, endday, nation, contentjson.count)
+   
+    respondJson(monetization)
+    
+    
+    //val Itemdata = Map("toplist" -> toplist, "itemlist" -> itemlists)
+
+  }
+}
+
+@GET("/monetization/item")
+class GetMonetizationItemList extends Api2 {
+
+  def execute() {
+    val appid = param[String]("appid")
+    val startday = param[String]("startday")
+    val endday = param[String]("endday")
+    val nation = param[String]("nation")
+    
+    val monetization = MonetizationDao.GetItemList(appid, startday, endday, nation)
+   
+    respondJson(monetization)
+  }
+}
+
---
+++ src/main/scala/haru/dao/MonetizationDao.scala
@@ -0,0 +1,96 @@
+package haru.dao
+
+import scala.slick.driver.MySQLDriver.simple._
+import scala.slick.lifted.ProvenShape
+import scala.slick.lifted.Tag
+import java.sql.SQLIntegrityConstraintViolationException
+import scala.slick.jdbc.{ GetResult, StaticQuery => Q }
+import Q.interpolation
+
+object MonetizationDao extends DatabasePool {
+
+  def GetMonetization(appid: String, startday: String, endday: String, nation: String, usercount: Int): List[Map[String, Any]] = databasePool withSession {
+    implicit session =>
+      /// TODO 만약없다면 exception??
+      /*
+      select buydate, sum(price), sum(price) / count(userid), count(userid), $usercount
+      from Monetization 
+      where applicationid = $appid and buydate between $startday and $endday
+      group by buydate
+      */
+      val query = sql"""
+      select buydate,  sum(sumprice) totalprice,  avg(avgprice) avgprice, count(userid) usercount, $usercount, sum(cnt) count
+	  from (
+	  select buydate, userid, sum(price) sumprice, avg(price) avgprice, count(userid) cnt
+	      from Monetization 
+	      where applicationid = $appid and buydate between $startday and $endday
+	      group by buydate, userid
+	  ) m group by buydate;
+       """.as[(String, Int, Double, Int, Int, Int)]
+
+      val list = query.list
+      var monetizations: List[Map[String, Any]] = List();
+      list.foreach { p =>
+        monetizations ++= List(Map("buydate" -> p._1, "totalprice" -> p._2, "avgprice" -> p._3, "countsalesuser" -> p._4, "usercount" -> p._5, "itemcount" -> p._6))
+      }
+      return monetizations
+  }
+
+  def GetItemList(appid: String, startday: String, endday: String, nation: String): Map[String, Any] = databasePool withSession {
+    implicit session =>
+      /// TODO 만약없다면 exception??
+      /*
+      기간별 판매 리스트 상위 5개 
+      sales item top 5 list 
+      */
+      val query = sql"""
+      select buydate, productname, price, count(productname), sum(price) 
+      from Monetization 
+      where applicationid = $appid and buydate between $startday and $endday
+      and productname in (
+      select * from (
+     	 select productname from Monetization 
+      	where applicationid = $appid and buydate between $startday and $endday
+      	group by productname limit 0, 5) as tmp
+      ) 
+      group by buydate, productname;
+       """.as[(String, String, Int, Int, Int)]
+
+      val list = query.list
+      var toplist: List[Map[String, Any]] = List();
+      list.foreach { p =>
+        toplist ++= List(Map("buydate" -> p._1, "productname" -> p._2, "price" -> p._3, "count" -> p._4, "total" -> p._5))
+      }
+
+      
+      val totalitem = sql"""
+      select sum(price) 
+      from Monetization 
+      where applicationid = $appid and buydate between $startday and $endday
+      """.as[(Int)]
+      val totals = totalitem.list
+      val totalprice = totals(0)
+     
+      
+      /*
+      sales item list
+      */
+      val itemquery = sql"""
+      select productname, price, sum(price), sum(price) / $totalprice * 100, count(price)
+      from Monetization 
+      where applicationid = $appid and buydate between $startday and $endday
+      group by productname order by sum(price) desc
+       """.as[(String, Int, Int, Double, Int)]
+
+      val itemlist = itemquery.list
+      var itemlists: List[Map[String, Any]] = List();
+      itemlist.foreach { p =>
+        itemlists ++= List(Map("productname" -> p._1, "price" -> p._2, "total" -> p._3, "avg" -> p._4, "count" -> p._5))
+      }
+
+      val Itemdata = Map("toplist" -> toplist, "itemlist" -> itemlists)
+
+      return Itemdata
+  }
+}
+// push_table.filter(p => p.appid === appid).sortBy(_.sendtime.desc).drop(page * limit).take(limit).run
