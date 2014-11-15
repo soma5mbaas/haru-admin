ea64dcf5bb104d26be6cc06619948d45cb326f48
parkjungun <pheadra4@gmail.com>
2014-11-16 05:23:04 +0900

    작성자 : 박중운 일시 : 2014.11.16 내용 : 1.redis release  namespace 변경

--- src/main/scala/haru/util/RedisKeyGenerator.scala
+++ src/main/scala/haru/util/RedisKeyGenerator.scala
@@ -2,10 +2,10 @@ package haru.util
 
 object RedisKeyGenerator {
   def getClass(appid: String): String = {
-    "ns:classes:" + appid
+    "rs:classes:" + appid
   }
 
   def getSchema(appid: String, classes: String): String = {
-    "ns:schema:" + classes + ":" + appid
+    "rs:schema:" + classes + ":" + appid
   }
 }
